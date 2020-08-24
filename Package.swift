487cc133bba57ba493f171e8079a13d698676abe
zmeriksen <zmeriksen@icloud.com>
2020-08-24 18:53:34 -0500

    Updated and removed tests

--- Package.swift
+++ Package.swift
@@ -17,7 +17,7 @@ let package = Package(
     dependencies: [
         // Dependencies declare other packages that this package depends on.
         // .package(url: /* package url */, from: "1.0.0"),
-        .package(url: "https://github.com/0xLeif/Later", from: "0.2.0"),
+        .package(url: "https://github.com/0xLeif/Later", from: "0.2.1"),
         .package(url: "https://github.com/0xLeif/SwiftUIKit", from: "1.3.0")
     ],
     targets: [
--- Tests/LaterViewTests/LaterViewTests.swift
+++ Tests/LaterViewTests/LaterViewTests.swift
@@ -1,18 +1,8 @@
 import XCTest
-@testable import LaterView
 
 final class LaterViewTests: XCTestCase {
     func testExample() {
-//        let laterView = UIView.later { (later) in
-//            later.main(withDelay: 3) {
-//                UILabel()
-//                    .configure { $0.text = "Hello World!" }
-//            }
-//        }
-//        
-//        sleep(3)
-//        
-//        XCTAssertEqual(laterView.text, "...")
+        XCTAssert(true)
     }
     
     static var allTests = [
